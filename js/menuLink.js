1d4151b828d29403bf2431575f611fa7aa9d8951
George Erokhin <Georgii070684@gmail.com>
2021-12-11 00:46:05 +0300

    feat menulink part 2

--- js/menuLink.js
+++ js/menuLink.js
@@ -1,16 +1,24 @@
-import {getTop} from "./services.js";
-import {getPopular } from "./services.js";
+import { getTop } from "./services.js";
+import { getPopular } from "./services.js";
 import renderCard from "./renderCard.js";
 
 const title = document.querySelector(".other-films__title");
 const filmWeek = document.querySelector('.film-week');
 const getNav = document.querySelectorAll('.get-nav');
-console.log('getNav' , getNav);
+console.log('getNav', getNav);
 
-const menuLink =() => {
+const menuLink = () => {
     getNav.forEach(nav => {
+        nav.addEventListener('click', event => {
 
-    }) 
+            const target = event.target.closest('.get-nav__link');// если кликанье мима таргета (..get-nav__link') получаешь null, при кликанье на ссылку получаешь эту ссылку.,  
+            if (target) {
+                event.preventDefault();
+                filmWeek.style.display= 'none';
+                title.textContent= target.textContent;
+            }
+        })
+    })
 }
 
 export default menuLink;
